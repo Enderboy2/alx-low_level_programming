e77b4a53b68f721cfc9ec06a01efda11551b9f71
YoussefKarim <y01006452923@gmail.com>
2023-04-28 15:32:21 +0300

    .

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-gcc -c *.c
-ar rc liball.a *o
+
+SRC_FILES=$(ls *.c)
+
+for SRC_FILE in $SRC_FILES; do
+    gcc -Wall -Werror -Wextra -pedantic -c $SRC_FILE
+done
+
+ar rcs liball.a *.o
+
+rm *.o
