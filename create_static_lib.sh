f4da3e5b6350b468b6bf0ab3c47143e8d9bb7242
Enderboy2 <y01006452923@gmail.com>
2023-04-28 14:38:12 +0200

    .

--- create_static_lib.sh
+++ create_static_lib.sh
@@ -4,8 +4,8 @@ SRC_FILES=$(ls *.c)
 
 for SRC_FILE in $SRC_FILES; do
     gcc -Wall -Werror -Wextra -pedantic -c $SRC_FILE
-done
+    done
 
-ar rcs liball.a *.o
+    ar rcs liball.a *.o
 
-rm *.o
+    rm *.o
